a2841e0a2e74275d34c0413a0bb4a56f05366b6a
Artur Assis Alves <arturassiscomp@gmail.com>
2023-08-16 17:40:53 -0300

    Add the CLI commands with the options for help and version

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,19 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    description='A toolkit for data quality. It helps you to generate data quality'
+                ' reports about structured data.',
+    epilog='For more information, visit https://github.com/ArturAssisComp/data-quality-tools#readme',
+    )
+
+parser.add_argument("--version", action="version", version="data-quality-tools 0.0.1")
+
 def main():
-    print('hello world')
+    parser.parse_args()
+
+
+
 
 
 
