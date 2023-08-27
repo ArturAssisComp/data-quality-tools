007cff389598ce4261bcd265d0acb725e4c5e6a2
Artur Assis Alves <arturassiscomp@gmail.com>
2023-08-26 21:32:38 -0300

    update nvi arguments

--- src/tools/null_value_inspector/arguments.py
+++ src/tools/null_value_inspector/arguments.py
@@ -2,4 +2,3 @@ from argparse import ArgumentParser
 
 def add_arguments(tool_subparser:ArgumentParser):
     null_value_inspector_group = tool_subparser.add_argument_group('Tool: Null Value Inspector Settings')
-    null_value_inspector_group.add_argument("hello-world", type=str, help="Hello World")
