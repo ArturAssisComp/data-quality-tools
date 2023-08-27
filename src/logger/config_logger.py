48a2b58fe0e825e9b04997d5298c00a76bfb5db5
Artur Assis Alves <arturassiscomp@gmail.com>
2023-08-26 22:43:22 -0300

    update gitignore

--- src/logger/config_logger.py
+++ src/logger/config_logger.py
@@ -12,7 +12,7 @@ def configure_logger():
     stdout_handler.setFormatter(basic_formatter)
 
     # Add file handler
-    file_handler = logging.FileHandler(filename="log.txt", mode="a", encoding="utf-8")
+    file_handler = logging.FileHandler(filename="data-quality-tools.log", mode="a", encoding="utf-8")
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(basic_formatter)
 
