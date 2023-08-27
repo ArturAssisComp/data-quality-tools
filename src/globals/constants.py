25d7c6168505e0137c8cb58f0deaaeca30188408
Artur Assis Alves <arturassiscomp@gmail.com>
2023-08-26 22:41:06 -0300

    add logger configuration

---
+++ src/globals/constants.py
@@ -0,0 +1,20 @@
+from globals.interfaces import Immutable
+
+class CONSTANTS(Immutable):
+    class Logger(Immutable):
+        class Formatters(Immutable):
+            basic_log_formatter = {
+                    "format": "[%(asctime)s] [%(levelname)8s] --> (%(name)s) - \"%(message)s\"",
+                    "datefmt": "%Y-%m-%d %H:%M:%SZ",
+                }
+            error_log_formatter = {
+                    "format": """%(levelname)s -->
+                                Date/time:      %(asctime)s
+                                Process(PID):   %(processName)s (%(process)d)
+                                Module:         %(name)s
+                                File:           %(filename)s
+                                Function:       %(funcName)s
+                                At line:        %(lineno)d
+                                Description:   "%(message)s" """,
+                    "datefmt": "%Y-%m-%d %H:%M:%SZ",
+                }
