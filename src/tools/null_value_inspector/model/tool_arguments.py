0db67116d7bbe08c873247938d346d7dc356755d
Artur Assis Alves <arturassiscomp@gmail.com>
2023-10-26 22:38:20 -0300

    extract base tool class

--- src/tools/null_value_inspector/model/tool_arguments.py
+++ src/tools/null_value_inspector/model/tool_arguments.py
@@ -1,36 +1,7 @@
-import os
-from pydantic import BaseModel, field_validator
+from tools.base_tool_arguments import BaseToolArguments
 
-class ToolArguments (BaseModel):
-    tool_name: str
-    documentation: str = ''
-    dataset: list[str]
-    output_path: str = ''
+class ToolArguments (BaseToolArguments):
     ranked_null_count_by_column_overview:bool = False
     null_distribution_by_row_overview:bool = False
     statistical_summary_overview:bool = False
     null_frequent_pairs_overview:bool = False
-    sample: str = ''
-
-
-
-    @field_validator('dataset', mode='before')
-    def split_and_check_dataset(cls, v):
-        if isinstance(v, str):  # Convert string to list
-            v = list(map(str.strip, v.split(',')))
-            for path in v:
-                if not os.path.isfile(path) and not os.path.isdir(path):
-                    raise ValueError(f'{path} is not a valid path.')
-        else:
-            raise TypeError('Expected str type')
-        return v
-
-    @field_validator('output_path')
-    def check_output_path(cls, v):
-        if v and not os.path.isdir(v):
-            raise ValueError(f'{v} is not a valid path.')
-        return v
-
-    class Config:
-        extra = "ignore"
-
--- src/tools/null_value_inspector/null_value_inspector.py
+++ src/tools/null_value_inspector/null_value_inspector.py
@@ -2,13 +2,11 @@ import os
 import logging
 import json
 
-from globals.interfaces import BaseToolClass
-from globals.constants import CONSTANTS
+from tools.base_tool import  BaseToolClass
 from globals.types import SnapshotType
 from tools.null_value_inspector.model.tool_arguments import ToolArguments
 from tools.null_value_inspector.model.documentation import Documentation
 from logger.utils import log_footer, log_header, get_custom_logger_name
-from utils.str_operations import parse_samples
 
 # tools
 from tools.null_value_inspector.result_generator.null_distribution_by_row.generator import NullDistributionByRowOverviewGenerator
@@ -30,43 +28,13 @@ class NullValueInspector(BaseToolClass):
     _row_null_distribution_snapshot_path:str | None
     _column_null_count_snapshot_path:str | None
     _column_pair_null_pattern_snapshot_path:str | None
-    _base_snapshot_path:str
-    _base_result_path:str 
     _documentation:Documentation
-    _file_operations:FileOperations
-    _samples:list[str | int] | None
-    def __init__(self, file_operations:FileOperations=FileOperations()):
+    def __init__(self, logger:logging.Logger = logger, file_operations:FileOperations=FileOperations()):
         self._file_operations = file_operations
+        self._logger = logger
 
 
-    def work_on(self, tool_arguments: ToolArguments):
-        self._base_snapshot_path = os.path.join(tool_arguments.output_path, CONSTANTS.FilesFoldersNames.snapshot)
-        self._base_result_path = os.path.join(tool_arguments.output_path, CONSTANTS.FilesFoldersNames.results)
-        documentation_path = tool_arguments.documentation
 
-        self._documentation = self._get_documentation(documentation_path)
-
-        # parse samples
-        self._samples = self._get_samples_specification(tool_arguments.sample)
-
-        # create the snapshots
-        self._create_snapshots(tool_arguments)
-
-        # create the results
-        self._create_results(tool_arguments)
-
-    def _get_samples_specification(self, sample_arg:str):
-        if sample_arg:
-            try:
-                result =  parse_samples(sample_arg)
-                if result:
-                    return result
-                raise ValueError('Empty result')
-            except Exception as e:
-                logger.error(f'Invalid argument samples ({sample_arg}): {e}')
-                raise
-        else:
-            return None
         
 
     def _get_documentation(self, documentation_path:str):
@@ -171,7 +139,6 @@ class NullValueInspector(BaseToolClass):
                 logger.error('Invalid column_pair_null_pattern_snapshot')
         log_footer(logger, 'Results Finished    ')
     
-    def _snapshot_is_available(self, snapshot_path:str|None):
-        return snapshot_path and os.path.isfile(snapshot_path)
+
 
 
