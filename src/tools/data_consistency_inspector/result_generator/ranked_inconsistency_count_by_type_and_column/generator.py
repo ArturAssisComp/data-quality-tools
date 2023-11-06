34aa2f9e83957e670c02ca03783c189fac8457f0
Artur Assis Alves <arturassiscomp@gmail.com>
2023-11-06 17:31:55 -0300

    implement generator

--- src/tools/data_consistency_inspector/result_generator/ranked_inconsistency_count_by_type_and_column/generator.py
+++ src/tools/data_consistency_inspector/result_generator/ranked_inconsistency_count_by_type_and_column/generator.py
@@ -2,7 +2,6 @@ import logging
 import os
 from typing import Any
 import matplotlib.pyplot as plt
-import numpy as np
 from globals.types import SnapshotType
 
 from logger.utils import get_custom_logger_name
@@ -14,23 +13,10 @@ from utils.plot_operations import PlotOperations
 logger = logging.getLogger(get_custom_logger_name(__name__, len(__name__.split('.')) - 2, 'last'))
 
 # constants
-BAR_PLOT_SETTINGS = {
-    'relative': {
-        'y_label': 'Percentage of Inconsistencies',
-        'fig_name': 'rel_ranked_inconsistency_count_by_type_and_column_overview.png',
-        'suffix': '%'
-    },
-    'absolute': {
-        'y_label': 'Row Count',
-        'fig_name': 'abs_ranked_inconsistency_count_by_type_and_column_overview.png',
-        'suffix': ''
-    }
-}
-CONTENT_SIZE_LIMIT_FOR_TOP_LABEL = 20
-CONTENT_SIZE_LIMIT_FOR_X_LABEL = 50
-VALUE_SIZE_LIMIT = 1000
+FIG_NAME = 'abs_ranked_inconsistency_count_by_type_and_column_overview.png'
+Y_LABEL = 'Inconsistencies Count'
 X_LABEL = 'Inconsistency Per Column'
-TITLE = 'Ranked Inconsistency Count by Column Overview'
+TITLE = 'Ranked Inconsistency Count by Column and Type Overview'
 
 
 class RankedInconsistencyCountByTypeAndColumnOverviewGenerator(BaseOverviewGenerator):
@@ -48,101 +34,93 @@ class RankedInconsistencyCountByTypeAndColumnOverviewGenerator(BaseOverviewGener
                 self._generate_bar_plot(column_inconsistency_count_by_type_snapshot_content.content, basedir_path, name_prefix=name_preffix)
             finally:
                 plt.close()
-            try:
-                self._generate_bar_plot(column_inconsistency_count_by_type_snapshot_content.content, basedir_path, relative=True, name_prefix=name_preffix)
-            finally:
-                plt.close()
         except Exception as e:
             self._logger.error(f'Result not generated: {e}')
 
 
-    def _should_add_label_on_top(self, content:dict, relative:bool):
-        basic_check = len(content) <= CONTENT_SIZE_LIMIT_FOR_TOP_LABEL 
-        if relative:
-            return basic_check
-        return basic_check and max(content.values()) < VALUE_SIZE_LIMIT
-
-    '''
-    # TODO refactor: extract this function, because it is implemented also in null value inspector tool
-    def _generate_bar_plot(self,column_inconsistency_count_snapshot_content:ColumnInconsistencyCountByTypeSnapshotContent, basedir_path:str, name_preffix:str='', relative:bool=False):
-        content = column_inconsistency_count_snapshot_content.content
-
-        total_nulls = sum(values)
-        total_columns = len(keys)
-        average_value = 100 / total_columns if relative else total_nulls / total_columns
-
-        x = list(keys)
-        y = [value/total_nulls * 100 for value in values] if relative else values
-        
-        if len(y) > CONTENT_SIZE_LIMIT_FOR_X_LABEL:
-            x = x[:CONTENT_SIZE_LIMIT_FOR_X_LABEL]
-            y = y[:CONTENT_SIZE_LIMIT_FOR_X_LABEL]
-                
-
-        
-        settings_key = 'relative' if relative else 'absolute'
-        settings = BAR_PLOT_SETTINGS[settings_key]
-        
-        has_label_on_top = self._should_add_label_on_top(content, relative)
-
-        self._plot_operations.plot_figure(list(range(len(x))), y, settings['y_label'], X_LABEL, TITLE, average_value, has_label_on_top, name_preffix + settings['fig_name'], relative, settings['suffix'], basedir_path, x_labels=x)
-
-        self._logger.info(f'{settings["fig_name"]} generated!')
-    '''
-
-
-    def _generate_bar_plot(self, content: dict, basedir_path: str, name_prefix: str='', relative: bool=False):
-            groups = []
-            group_values = {}
-
-            # Collect all subgroups with non-zero values and their sums across groups
-            for group, sub_content in content.items():
-                groups.append(group)
-                for sub_group, value in sub_content.items():
-                    if value != 0:  # Exclude subgroups with zero value
-                        if sub_group not in group_values:
-                            group_values[sub_group] = []
-                        group_values[sub_group].append((group, value))
-
-            # Sort subgroups by the sum of their values
-            sorted_sub_groups = sorted(group_values, key=lambda x: sum(val for _, val in group_values[x]), reverse=True)
-
-            # Create a figure and a set of subplots
-            fig, ax = plt.subplots()
-
-            # Calculate the bar positions
-            num_groups = len(groups)
-            total_width = 0.8  # Total width for all bars in one group
-            bar_width = total_width / len(sorted_sub_groups)
-
-            for i, sub_group in enumerate(sorted_sub_groups):
-                sub_group_data = group_values[sub_group]
-                sub_group_data.sort(key=lambda x: groups.index(x[0]))  # Sort by the group order
-                x = np.arange(num_groups)
-                heights = [0] * num_groups  # Start with zero heights for each group
-                
-                for group, value in sub_group_data:
-                    group_index = groups.index(group)
-                    heights[group_index] = value
-
-                offsets = (i - len(sorted_sub_groups) / 2) * bar_width + bar_width / 2
-                ax.bar(x + offsets, heights, bar_width, label=sub_group)
-
-            # Add some text for labels, title, and custom x-axis tick labels
-            ax.set_xlabel('Groups')
-            ax.set_ylabel('Values')
-            ax.set_title('Your Title Here')
-            ax.set_xticks(x)
-            ax.set_xticklabels(groups, rotation=45)
-            ax.legend()
-
-            # Save the plot
-            filename = f"{name_prefix}bar_plot.png"
-            fig_path = os.path.join(basedir_path, filename)
-            plt.savefig(fig_path, bbox_inches='tight')
-            plt.close(fig)  # Close the figure to free memory
-            
-            self._logger.info(f'Bar plot generated at {fig_path}!')
 
 
 
+    
+
+    def _generate_bar_plot(self, content: dict, basedir_path: str, name_prefix: str=''):
+        """
+        Generates a bar plot from the given content and saves it to a specified directory.
+
+        Parameters:
+        content (dict): A dictionary of main groups with subgroups as keys and their values.
+        basedir_path (str): The base directory path where the plot will be saved.
+        name_prefix (str): Optional prefix for the saved plot filename.
+        """
+        # Flatten the content into a list of (main_group, sub_group, value) tuples, excluding zero values
+        flat_content = [(main_group, sub_group, value) 
+                        for main_group, subgroups in content.items()
+                        for sub_group, value in subgroups.items() if value != 0]
+
+        # Sort and get unique subgroups across all main groups
+        all_subgroups = sorted({sub_group for _, sub_group, _ in flat_content}, )
+
+        # Start plotting
+        fig, ax = plt.subplots()
+        group_positions = []  # Store the x position for each main group
+        subgroup_labels = []  # Store the x position and label for each subgroup
+
+        current_position = 0  # Start at position 0 on the x-axis
+
+        # Go through each main group and its subgroups
+        for sub_content in content.values():
+            # Sort subgroups by the order in all_subgroups
+            subgroups = sorted(((k, v) for k, v in sub_content.items() if v != 0), key=lambda item: all_subgroups.index(item[0]), reverse=True)
+
+            # Calculate width for each subgroup bar
+            num_subgroups = len(subgroups)
+            width_per_subgroup = 0.8 / max(len(all_subgroups), num_subgroups) if num_subgroups else 0.8
+            subgroup_start_position = current_position - (0.8 / 2) + (width_per_subgroup / 2)
+
+            # Plot each subgroup
+            for i, (sub_group, value) in enumerate(subgroups):
+                x_position = subgroup_start_position + i * width_per_subgroup
+                ax.bar(x_position, value, width_per_subgroup, label=sub_group if not group_positions else "", align='center')
+                subgroup_labels.append((x_position, sub_group))
+
+            group_positions.append(current_position)
+            current_position += 1
+
+        # Set the x-axis labels for subgroups and groups
+        self._set_axis_labels(ax, group_positions, subgroup_labels, content.keys())
+
+        # Add the y-axis label and title
+        ax.set_ylabel(Y_LABEL)
+        ax.set_title(TITLE)
+
+
+        # Save the plot
+        self._save_plot(fig, basedir_path, name_prefix)
+
+    def _set_axis_labels(self, ax, group_positions, subgroup_labels, group_labels):
+        """
+        Sets the axis labels for the plot.
+        """
+        ax.set_xticks([pos for pos, label in subgroup_labels])
+        ax.set_xticklabels([label for pos, label in subgroup_labels], rotation=45, ha="right")
+
+        # Create a second x-axis for the main group labels
+        ax2 = ax.twiny()
+        ax2.set_xlim(ax.get_xlim())
+        ax2.set_xticks(group_positions)
+        ax2.set_xticklabels(group_labels)
+        ax2.xaxis.set_ticks_position('bottom')
+        ax2.xaxis.set_label_position('bottom')
+        ax2.spines['bottom'].set_position(('outward', 40))
+        ax2.tick_params(axis='x', which='major', pad=15)
+
+    def _save_plot(self, fig, basedir_path, name_prefix):
+        """
+        Saves the plot to the specified directory.
+        """
+        filename = f'{name_prefix}{FIG_NAME}'
+        fig_path = os.path.join(basedir_path, filename)
+        plt.tight_layout()
+        plt.savefig(fig_path, bbox_inches='tight')
+        plt.close(fig)
+        self._logger.info(f'{FIG_NAME} generated!')
